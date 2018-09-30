4ff95969aafbabee2d263fc1dfb380cf962d9278
CallumChaney <callumchaney8@gmail.com>
2018-09-30 17:34:10 +0100

    Add auth

---
+++ src/models/users.model.js
@@ -0,0 +1,40 @@
+// See http://docs.sequelizejs.com/en/latest/docs/models-definition/
+// for more of what you can do here.
+const Sequelize = require('sequelize');
+const DataTypes = Sequelize.DataTypes;
+
+module.exports = function(app) {
+  const sequelizeClient = app.get('sequelizeClient');
+  const users = sequelizeClient.define('users', {
+
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      isEmail: true,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    isCoach: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+  }, {
+    hooks: {
+      beforeCount(options) {
+        options.raw = true;
+      }
+    }
+  });
+
+  // eslint-disable-next-line no-unused-vars
+  users.associate = function(models) {
+    users.coachId = users.belongsTo(models.users, {
+      as: "coach"
+    })
+  };
+
+  return users;
+};
