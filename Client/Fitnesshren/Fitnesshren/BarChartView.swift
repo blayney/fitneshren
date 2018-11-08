bcc95ba170c15df0d0392776da0a8f2a673ea25b
Sweeper777 <sumulang@gmail.com>
2018-11-08 07:25:49 +0000

    calculated some stuff in draw

--- Client/Fitnesshren/Fitnesshren/BarChartView.swift
+++ Client/Fitnesshren/Fitnesshren/BarChartView.swift
@@ -10,4 +10,17 @@ class BarChartView: UIView {
     
     let barWidth = 12.f
     
+    override func draw(_ rect: CGRect) {
+        guard let percentages = self.percentages else { return }
+        
+        let graphHeight = height * 8.0 / 9.0
+        let separator = (width - barWidth * percentages.count.f) / (percentages.count.f + 1)
+        let weekdays = getLast7Weekdays(date: Date())
+        for (index, bar) in percentages.enumerated() {
+            let barX = (index + 1).f * separator + index.f * barWidth
+            let barY = graphHeight * (1.0 - bar.0).f
+            let barHeight = graphHeight - barY
+            let subBars = [(0, bar.1), (1, bar.2), (2, bar.3)].sorted(by: { $0.1 > $1.1 })
+        }
+    }
 }
