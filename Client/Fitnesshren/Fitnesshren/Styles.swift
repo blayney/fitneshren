3957824cec6b631688c082c4cb9b231af291be3b
Sweeper777 <sumulang@gmail.com>
2018-11-09 17:10:55 +0000

    added color styles

---
+++ Client/Fitnesshren/Fitnesshren/Styles.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+extension UIColor {
+    
+    @nonobjc class var pinkishTan: UIColor {
+        return UIColor(red: 233.0 / 255.0, green: 167.0 / 255.0, blue: 159.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var paleRed: UIColor {
+        return UIColor(red: 214.0 / 255.0, green: 67.0 / 255.0, blue: 49.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var veryLightPink: UIColor {
+        return UIColor(white: 238.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var darkPeach: UIColor {
+        return UIColor(red: 221.0 / 255.0, green: 110.0 / 255.0, blue: 97.0 / 255.0, alpha: 1.0)
+    }
+    
+}
