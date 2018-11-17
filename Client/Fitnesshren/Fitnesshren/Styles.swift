ff203603bbc1867b2a1a6d56389cd4838a89a0a7
Sweeper777 <sumulang@gmail.com>
2018-11-17 08:39:58 +0000

    added more colors

--- Client/Fitnesshren/Fitnesshren/Styles.swift
+++ Client/Fitnesshren/Fitnesshren/Styles.swift
@@ -18,6 +18,24 @@ extension UIColor {
         return UIColor(red: 221.0 / 255.0, green: 110.0 / 255.0, blue: 97.0 / 255.0, alpha: 1.0)
     }
     
+    @nonobjc class var veryPaleGray: UIColor {
+        return UIColor(white: 245.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var dirtyPurple: UIColor {
+        return UIColor(red: 81.0 / 255.0, green: 58.0 / 255.0, blue: 80.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var paleGold: UIColor {
+        return UIColor(red: 254.0 / 255.0, green: 204.0 / 255.0, blue: 110.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var purplishGrey: UIColor {
+        return UIColor(red: 105.0 / 255.0, green: 98.0 / 255.0, blue: 109.0 / 255.0, alpha: 1.0)
+    }
+
+
+    
 }
 
 // Text styles
