eb878503d0f2f02ad81ecb4327d2a1113b65d7a5
Sweeper777 <sumulang@gmail.com>
2018-11-17 08:42:16 +0000

    tested authentication

--- Client/Fitnesshren/Fitnesshren/ViewController.swift
+++ Client/Fitnesshren/Fitnesshren/ViewController.swift
@@ -1,20 +1,36 @@
-//
-//  ViewController.swift
-//  Fitnesshren
-//
-//  Created by Mulang Su on 2018/10/04.
-//  Copyright © 2018年 Mulang Su. All rights reserved.
-//
-
 import UIKit
+import SocketIO
+import FeathersSwiftSocketIO
+import Feathers
 
 class ViewController: UIViewController {
-
+    
+    let manager = SocketManager(socketURL: URL(string: "https://ilich.ml")!, config: [.log(false), .compress])
+    var feathers: Feathers!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print(UIFont.fontNames(forFamilyName: "SF Pro Display"))
         
         print((view.subviews.first! as! UILabel).font!.fontName)
+        
+        let provider = SocketProvider(manager: manager)
+        feathers = Feathers(provider: provider)
+        feathers.authenticate([
+            "strategy":"local","email": "admin", "password": "admin"
+            ]).on(starting: {print("started")},
+                  started: {print("starting")},
+                  event: { (event) in print("event: \(event)")},
+                  failed: {print($0.localizedDescription)},
+                  completed: {print("completed")},
+                  interrupted: {print("interrupted")},
+                  terminated: {print("terminated")},
+                  disposed: {print("disposed")},
+                  value: { [weak self] accessToken in
+                    print("value: \(accessToken)")
+                    self?.getStuff()
+            }).start()
+
     }
 
 
